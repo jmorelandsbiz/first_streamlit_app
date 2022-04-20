b581e0f7b039df6e1cb1b5fe972435155cf156bf
Jeremy Moreland <9988053+jmorelandsbiz@users.noreply.github.com>
2022-04-20 14:01:12 -0500

    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
-streamlit.title('My parents new healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
 
