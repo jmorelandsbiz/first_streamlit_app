775d07e03ad4bb502ad2a9c40c6bc0d58054fd71
Jeremy Moreland <9988053+jmorelandsbiz@users.noreply.github.com>
2022-04-21 21:40:55 -0500

    move stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
+
 
 # Snowflake functions
 
@@ -56,6 +56,8 @@ if streamlit.button("Get Fruit Load List"):
   streamlit.header("The fruit load list contains:")
   streamlit.dataframe(my_data_rows)
 
+  
+streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit');")
